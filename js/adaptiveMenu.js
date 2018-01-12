9c498ddc09d26094cfebfe0b27371e7167622bd0
reazy015 <redshuhart015@gmail.com>
2018-01-12 19:24:28 +0300

    day4

--- js/adaptiveMenu.js
+++ js/adaptiveMenu.js
@@ -3,11 +3,32 @@
 window.adaptiveMenu = (function () {
     var menuList = document.querySelector('.nav-list');
     var menuToggleBtn = document.querySelector('.nav-toggle');
+    var mainNav = document.querySelector('.nav-list');
 
-    function toggleMenu () {
+    function toggleMenu() {
        menuList.classList.toggle('nav-open');
+       menuList.classList.toggle('animation-nav');
+    }
+
+    function hideAllSubmenu() {
+        document.querySelectorAll('.submenu').forEach(function(item){
+            item.style.display = 'none';
+        })
+    }
+
+
+    function submenuHandler(evt) {
+        var target = evt.target;
+
+        if (target.classList.contains('nav-list-item--has-sub') && mainNav.classList.contains('nav-open')) {
+            var childSubmenu = target.querySelector('.submenu');
+            hideAllSubmenu();
+            childSubmenu.style.display = 'block';
+        } else {
+            hideAllSubmenu();
+        }
     }
 
     menuToggleBtn.addEventListener('click', toggleMenu);
-
+    mainNav.addEventListener('click', submenuHandler);
 })()
