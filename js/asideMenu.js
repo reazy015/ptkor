61250402806788e27184cd1d1f9b755801b165d0
reazy015 <redshuhart015@gmail.com>
2018-01-31 19:53:04 +0300

    3dweek 2days more deadline

--- js/asideMenu.js
+++ js/asideMenu.js
@@ -1,5 +1,32 @@
 'use strict';
 
 window.asideMenu = ( function () {
+  var asideToggleBtn = document.querySelector('.aside-nav-toggle');
+  var asideMenu = document.querySelector('.aside-nav');
+  var asideNavList = asideMenu.querySelector('.aside-nav-list');
+
+  function toggleAsideMenu(e) {
+    if (e.target == asideToggleBtn) {
+      asideMenu.classList.toggle('aside-nav--open');
+      window.addEventListener('click', closeAsideOnOutsideClick);
+    }
+  }
+
+  function closeAsideOnOutsideClick(e) {
+    if (e.target != asideToggleBtn) {
+      asideMenu.classList.remove('aside-nav--open');
+      window.removeEventListener('click', toggleAsideMenu);
+    }
+  }
+
+  function openAsideNavList() {
+    if (window.innerWidth < 830) {
+      asideNavList.classList.toggle('aside-nav-list--open');
+    }
+  }
+
+
+  asideToggleBtn.addEventListener('click', toggleAsideMenu);
+  asideMenu.addEventListener('click', openAsideNavList);
 
 })()
